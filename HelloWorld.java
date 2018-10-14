12f84d552fb39cad593acbee4af771515f6bd2c6
daovinhphat <43946799+daovinhphat@users.noreply.github.com>
2018-10-14 17:04:24 +0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class HelloWorld{
     
 	public static void TDTU(){
-		System.out.println("Hello World I am TDTU");
+		System.out.println("Hello World This is TDTU");
 	}
 	
 	public static void main(String[] args) {
