515388dcef39f22ddc8d0f694bd76eabcf3a43b0
daovinhphat <43946799+daovinhphat@users.noreply.github.com>
2018-10-14 16:55:23 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HelloWorld{
+    
+	public static void Master(){
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		Master();
+    }
+}
