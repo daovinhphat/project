5e29ee0cf319a13bd093100e41d6e06b4270d0d9
daovinhphat <43946799+daovinhphat@users.noreply.github.com>
2018-10-14 17:50:18 +0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,22 @@
 import java.util.*;
+import java.io.*;
 
 public class HelloWorld{
-    
 	public static void TDTU(){
 		System.out.println("Hello World This is TDTU");
-  	}
-
-	public static void Master(){
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Input your name: ");
-		String name;
-		name = sc.nextLine();
-		System.out.println("Hello World I am " + name);
-
+	}
+	public static void Master() throws IOException{
+		//Scanner sc = new Scanner(System.in);
+		//System.out.print("Input your name: ");
+		FileInputStream name = new FileInputStream("name.txt");
+		Scanner sc = new Scanner(name,"UTF-8");
+		while (sc.hasNextLine())
+			System.out.println("Hello World I am " + sc.nextLine());
+		sc.close();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		TDTU();
-   		 Master();
-    	}
+		Master();
+  }
 }
