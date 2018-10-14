8aff0019f38ca3de733f4a5d5f69bfee2b6d52d5
daovinhphat <43946799+daovinhphat@users.noreply.github.com>
2018-10-14 17:00:12 +0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class HelloWorld{
     
-	public static void Master(){
-		System.out.println("Hello World I am Dao Vinh Phat");
+	public static void TDTU(){
+		System.out.println("Hello World I am TDTU");
 	}
 	
 	public static void main(String[] args) {
-		Master();
+		TDTU();
     }
 }
