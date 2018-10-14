c800bed675d3c3a919e366a03dbcf3fa1163ae75
daovinhphat <43946799+daovinhphat@users.noreply.github.com>
2018-10-14 17:53:31 +0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,11 +8,15 @@ public class HelloWorld{
 	public static void Master() throws IOException{
 		//Scanner sc = new Scanner(System.in);
 		//System.out.print("Input your name: ");
+		int count = 0;
 		FileInputStream name = new FileInputStream("name.txt");
 		Scanner sc = new Scanner(name,"UTF-8");
-		while (sc.hasNextLine())
+		while (sc.hasNextLine()){
 			System.out.println("Hello World I am " + sc.nextLine());
+			count++;
+		}
 		sc.close();
+		System.out.println("Total " + count + " Students");
 	}
 	
 	public static void main(String[] args) throws IOException {
